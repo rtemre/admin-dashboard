951ed56995cfbcf6cd7a221c929ae077c894410e
Raman Temre <ramantemre2110@gmail.com>
2025-08-13 14:56:07 +0530

    Update eslint rules

--- eslint.config.js
+++ eslint.config.js
@@ -19,5 +19,12 @@ export default tseslint.config([
       ecmaVersion: 2020,
       globals: globals.browser,
     },
+    rules: {
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+      'react-refresh/only-export-components': 'off',
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+      '@typescript-eslint/no-explicit-any': 'off',
+    },
   },
 ])
